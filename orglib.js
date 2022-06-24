c666c1148b91f919cfde516373c1cc943338c670
Pim Snel <post@pimsnel.com>
2022-06-24 12:10:37 +0200

    fix redirect

--- orglib.js
+++ orglib.js
@@ -1,7 +1,7 @@
 function getURL() {
-  let _arr =window.location.href.split('repo=');
-  const link = _arr[1].split("://");
-  if (link) {
+  let _arr = window.location.href.split('repo=');
+  if (_arr.length > 1 ) {
+    const link = _arr[1].split("://");
     window.location.href = "quiqr://"+link;
     document.getElementById("repourl").innerHTML='<p><a href="quiqr://'+link[1]+'?proto='+link[0]+'">Open in Quiqr</a></p>';
   }
