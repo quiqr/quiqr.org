dfe89b4139ee7fa6e0068169cbc5100b28da5d72
mipmip <mipmip@users.noreply.github.com>
2023-06-06 17:40:49 +0000

    deploy: 1ed3589a10b1d73eb9e06f44bf6943ad3aa79e30

---
+++ orglib.js
@@ -0,0 +1,15 @@
+function getURL() {
+  let _arr = window.location.href.split('repo=');
+  if (_arr.length > 1 ) {
+    const link = _arr[1].split("://");
+    window.location.href = "quiqr://"+link;
+    document.getElementById("repourl").innerHTML='<p><a href="quiqr://'+link[1]+'?proto='+link[0]+'">Open in Quiqr</a></p>';
+  }
+  else{
+    window.location.href = "https://open.quiqr.org";
+  }
+}
+
+addEventListener("load", function(){
+  getURL();
+});
